35015f0447b1f6779b21b124a30163ef8e530a66
unknown <anandteertha.ramesh18@siesgst.ac.in>
2022-07-07 11:30:22 +0530

    compare first version

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from "../app.component";
+import {Injectable, Renderer2, RendererFactory2, ViewChild, ElementRef, Input} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-compare',
@@ -6,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare.component.css']
 })
 export class CompareComponent implements OnInit {
-
-  constructor() { }
+  field: {
+    CurrencyName: string;
+    Country: string;
+    USD_Equivalent: string;
+  }
+  fields: Array<any>;
+  constructor(public AppComponent: AppComponent, private renderer: Renderer2, private route: ActivatedRoute, private router: Router) {
+    this.fields = this.AppComponent.fields;
+  }
 
   ngOnInit(): void {
   }
